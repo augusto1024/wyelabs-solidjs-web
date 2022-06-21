02ac6f2f54713e3aa11d1214ac4fbeeacfdd1d98
Augusto Alonso <augusto1024@gmail.com>
2022-06-21 11:56:41 -0300

    Add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ac001d'
+      }
+    },
   },
   plugins: [],
 }
