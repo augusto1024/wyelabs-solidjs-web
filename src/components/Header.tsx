7dfdceaea858f29db2915c6f41fd06b5d05a10b9
Augusto Alonso <augusto1024@gmail.com>
2022-06-21 14:05:31 -0300

    Add header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
 import { JSX } from 'solid-js/jsx-runtime';
+import WyeworksLogo from '../assets/logo.png';
 
-export const Header = (): JSX.Element => {
-  return <div>Header Component</div>;
-};
+export const Header = (): JSX.Element => (
+  <header>
+    <img src={WyeworksLogo} />
+  </header>
+);
