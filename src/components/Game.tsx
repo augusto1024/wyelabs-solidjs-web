8e19095fa3366c425a0be1330289fd9534c1cefc
Augusto Alonso <augusto1024@gmail.com>
2022-06-21 14:34:36 -0300

    Populate options component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,15 +1,33 @@
-import { createResource } from 'solid-js';
+import { createEffect, createResource, createSignal } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
 import { Image, Options } from '.';
 import { fetchPeople } from '../utils/requests';
 
 export const Game = (): JSX.Element => {
   const [data, { refetch }] = createResource<Person[]>(fetchPeople);
+  const [selectedPerson, setSelectedPerson] = createSignal<Person>();
+
+  createEffect(() => {
+    const people = data();
+
+    if (data.loading || !people) {
+      setSelectedPerson(undefined);
+    } else {
+      const index = Math.floor(Math.random() * (people.length - 1));
+      setSelectedPerson(people[index]);
+    }
+  });
+
+  const handlePersonSelection = (person: Person): boolean =>
+    person === selectedPerson();
 
   return (
     <div class="game">
       <Image />
-      <Options />
+      <Options
+        people={data.loading ? [] : (data() as Person[])}
+        onSelection={handlePersonSelection}
+      />
       <button onClick={refetch} disabled={data.loading} class="primary">
         Obtener WyeWorker al azar
       </button>
--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,5 +1,54 @@
+import { Show, For, createSignal } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
 
-export const Options = (): JSX.Element => {
-  return <div class="options">Options Component</div>;
+interface OptionsProps {
+  people: Person[];
+  onSelection: (person: Person) => boolean;
+}
+
+type Selection = {
+  correct: boolean;
+  person?: Person;
+};
+
+export const Options = (props: OptionsProps): JSX.Element => {
+  const [selection, setSelection] = createSignal<Selection>({ correct: false });
+
+  const handleSelection = (person: Person): void => {
+    const correct = props.onSelection(person);
+    setSelection({
+      person,
+      correct,
+    });
+  };
+
+  return (
+    <div class="options">
+      <Show when={props.people.length}>
+        <For each={props.people}>
+          {(person) => (
+            <button
+              classList={{
+                correct: selection().person === person && selection().correct,
+                wrong: selection().person === person && !selection().correct,
+              }}
+              onClick={() => handleSelection(person)}
+              class="secondary"
+            >
+              {person.name}
+            </button>
+          )}
+        </For>
+      </Show>
+      <Show when={!props.people.length}>
+        <For each={[1, 2, 3, 4]}>
+          {() => (
+            <button disabled>
+              <div>Cargando...</div>
+            </button>
+          )}
+        </For>
+      </Show>
+    </div>
+  );
 };
