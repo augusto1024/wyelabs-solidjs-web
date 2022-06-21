1aef0c604f5479effc8145c6c5a25b614b71b510
Augusto Alonso <augusto1024@gmail.com>
2022-06-21 14:19:52 -0300

    Add button for fetching people

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,18 @@
+import { createResource } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
 import { Image, Options } from '.';
+import { fetchPeople } from '../utils/requests';
 
 export const Game = (): JSX.Element => {
+  const [data, { refetch }] = createResource<Person[]>(fetchPeople);
+
   return (
     <div class="game">
       <Image />
       <Options />
+      <button onClick={refetch} disabled={data.loading} class="primary">
+        Obtener WyeWorker al azar
+      </button>
     </div>
   );
 };
